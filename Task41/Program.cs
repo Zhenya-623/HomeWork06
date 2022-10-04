bfc58e2fc7eba2947590b970eaebb3a45d1d19f0
Евгений Политыко <Zhenya-623@internet.ru>
2022-10-04 19:24:11 +0300

    task41 ready

---
+++ Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+//метод преобразования массива с испольованием функции split
+int[] ConvertArray(string arrayStr)
+{
+    // преобразовываем массив с использованием метода PrintArray
+    string [] array = arrayStr.Split(",");
+    int[] numarray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        numarray[i] = int.Parse(array[i]);
+    }
+    return numarray;
+}
+
+
+int FindPositiveNumber(int[] arr)
+{
+    int length = arr.Length;
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+
+
+//команда для ввода массива
+Console.WriteLine("Введите массив чисел отделяя каждую цифру запятой без пробелов:");
+//в переменную arrayStr передаем введенные числа
+string arrayStr = Console.ReadLine() ?? "";
+// преобразовываем массив с использованием метода PrintArray
+int[] result = ConvertArray(arrayStr);
+
+int countPositiveNumber = FindPositiveNumber(result);
+Console.WriteLine("Количество чисел больше нуля:  " + countPositiveNumber);
